b4826fa163666a26cffd244cf0410c6e27a49e25
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-22 17:27:55 -0500

    Needs comments

--- achieve_5.py
+++ achieve_5.py
@@ -1,1 +1,18 @@
+numbers = {
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {}
+}
+for base, power in numbers.items():
+    calcu = base ** 3
+    print(" {} to the power of 3 equals {}".format(base, calcu))
+ 
 
+    
