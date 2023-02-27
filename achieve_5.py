4da999610160b1b0650f12c69c6a2674879a19dc
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-26 19:43:29 -0500

    comments

--- achieve_5.py
+++ achieve_5.py
@@ -1,10 +1,10 @@
 #Name:          achieve_5.py
 #Author:        AJ Varatharajan
 #Date Created:  February 22, 2023
-#Date Last Modified: February 23, 2023
+#Date Last Modified: February 26, 2023
 #Purpose: Program will cycle through the values one to ten and will output each value expressed in cubed form.
 # 
-numbers = {
+numbers = { #creating dictionary
     1: {},
     2: {},
     3: {},
@@ -17,16 +17,14 @@ numbers = {
     10: {}
 }
 
-for base, power in numbers.items():
+for base, power in numbers.items(): #cycling through each entry in the dictionary
     
-    calcu = base ** 3  
-    
-    store = [list[base]]
-    store1 = [list[calcu]]
-    store2 = store.append(store1) 
-    numbers2 = {}
-    numbers2 = {base, calcu}
-    print(numbers2)
+    calcu = base ** 3  #Raising each entry to power of three and saving it to a variable
+    store = [list[base]] #Saving our base values as a list
+    store1 = [list[calcu]] #Saving our final calculation as a list
+    store2 = store.append(store1) #joining our two lists together
+    numbers2 = {} #creating an empty dictionary
+    numbers2 = {base, calcu} #creating an entry for each cycle
     print(store)
     
 
